9e63b53904c415f662f28aeba9254251c79525c5
BeatriceMani <92886561+BeatriceMani@users.noreply.github.com>
2022-06-01 13:50:09 -0700

    Create ClassroomLibrarianUtils.java

---
+++ src/main/java/util/ClassroomLibrarianUtils.java
@@ -0,0 +1,14 @@
+package util;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class ClassroomLibrarianUtils {
+    private static final int ID_LENGTH = 10;
+
+    private ClassroomLibrarianUtils() {}
+
+
+    public static String generateId() {
+        return RandomStringUtils.randomAlphanumeric(ID_LENGTH);
+    }
+}
